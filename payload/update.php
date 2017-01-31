26d94c8d59e47a8b3edabdf23b528b9e453e7b1b
Mirko Brunner <mirko.brunner@googlemail.com>
2017-01-31 16:01:09 +0100

    efw

--- payload/update.php
+++ payload/update.php
@@ -1,6 +1,19 @@
 <?php
 
-$x = exec("cd ../ git pull origin master", $i, $o);
-file_put_contents('test.log', print_r($x, 1));
+//$x = exec("sh pull.sh", $i, $o);
+$x = exec("git pull origin master", $i, $o);
+var_dump($x);
+echo '<pre>';
+var_dump($i);
+echo '</pre>';
+var_dump($o);
+
+if(is_array($i)) {
+	
+	if($i[0] == "Already up-to-date.") {
+		//do nothing
+		
+	}
+}
 
 ?>
