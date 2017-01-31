60d0bb160ff79523d3215bf3380303a2d80b1b77
Mirko Brunner <mirko.brunner@googlemail.com>
2017-01-31 17:01:11 +0100

    update php fix

--- payload/update.php
+++ payload/update.php
@@ -1,23 +1,27 @@
 <?php
-
 error_reporting(0);
 try {
         // Decode the payload json string
-    $payload = json_decode($_REQUEST['payload']);
-    
+		$payload = json_decode($_REQUEST['payload']);
+		
 } catch(Exception $e) {
 	
         exit(0);
 }
-// Pushed to master?
+// Pushed to master
 if ($payload->ref === 'refs/heads/master')
 {
         // Log the payload object
-        file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
-        // Prep the URL - replace https protocol with git protocol to prevent 'update-server-info' errors
-        $url = str_replace('https://', 'git://', $payload->repository->url);
+        
+        #file_put_contents('github.txt', print_r($payload, TRUE), FILE_APPEND);
+		
+		$where = $payload->commits[0]->committer->name;
+		$commit_msg  = $payload->commits[0]->message;
+		file_put_contents('github.txt', print_r($where."_".$commit_msg, TRUE), FILE_APPEND);
         // Run the build script as a background process
-        `./build.sh {$url} {$payload->repository->name} > /dev/null 2>&1 &`;
+        exec("git pull origin master", $r, $rr);
+		file_put_contents('github.txt', print_r($r, TRUE), FILE_APPEND);
+		file_put_contents('github.txt', print_r($rr, TRUE), FILE_APPEND);
 }
-//secret 834lafhinsdcluw39rowp0q2
+
 ?>
